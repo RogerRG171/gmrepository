5dcba346cdbb9755a4a481c98b64d3f97b6255d4
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2023-07-13 18:09:43 -0300

    Create dto, service, controller: findById GameDTO

--- src/main/java/com/goulart/dslist/services/GameService.java
+++ src/main/java/com/goulart/dslist/services/GameService.java
@@ -4,23 +4,32 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.goulart.dslist.dto.GameDTO;
 import com.goulart.dslist.dto.GameMinDTO;
 import com.goulart.dslist.entities.Game;
 import com.goulart.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
-	public List<GameMinDTO> findAll(){
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		List<GameMinDTO> dto = result
-				.stream()
-				.map(x -> new GameMinDTO(x))
-				.toList();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+
+		GameDTO dto = new GameDTO(result);
+		return dto;
+
 	}
 }
